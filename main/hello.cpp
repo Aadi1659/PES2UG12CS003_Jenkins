a83d4380ab3437a7b0469de1d8da9c7116d64a2c
Aaditya Dev Sharma <90966493+Aadi1659@users.noreply.github.com>
2024-03-05 18:45:50 +0530

    Create hello.cpp

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout<<"AADITYA DEV SHARMA PES2UG21CS003"
+  cout << "I have successfully built and run\n";\
+  return 0;
+}
