529e07ce6966949d111544631513788fdb754857
Aaditya Dev Sharma <90966493+Aadi1659@users.noreply.github.com>
2024-03-05 21:45:19 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<<"AADITYA DEV SHARMA PES2UG21CS003"
+  cout<<"AADITYA DEV SHARMA PES2UG21CS003";
   cout << "I have successfully built and run\n";\
   return 0;
 }
